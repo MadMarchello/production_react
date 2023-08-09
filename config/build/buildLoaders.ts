cae162da279adfec5441101b3ef0e3435571f51f
MadMarchello <mark1010_00@mail.ru>
2023-08-10 01:39:07 +0700

    Подключили React и настроили css в webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,12 +5,28 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     //порядок при котором лоадеры возвращаются в массиве имеет значение
     //поэтому лучше выносить отдельные лоадеры в переменные
     //чтобы мы всегда видели последовательность этих лоадеров в массиве
+
+
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
+
+    //если не используем ts, нужен babel-loader
     const typescriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     }
-   return [
+    return [
         typescriptLoader,
+        cssLoaders,
     ]
 }
