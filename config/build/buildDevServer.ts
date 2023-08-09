d075dc6813831c524f7e59e16a53d348d31a4e03
MadMarchello <mark1010_00@mail.ru>
2023-08-10 02:26:53 +0700

    Pоутинг Code splitting Lazy Suspence

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfigurations {
     return {
         port: options.port,
         open: true,//автоматиечски  открывает страницу в бразере при запуске приложения
+        historyApiFallback: true,
     }
 }
