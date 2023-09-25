829e9166ad353f130c3d8510c58ce4422beac0cb
MadMarchello <mark1010_00@mail.ru>
2023-09-25 21:39:35 +0300

    add new custom component NavBar with custom component Link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React, { Suspense, useContext, useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -14,8 +12,7 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>{theme}</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
         </div>
     );
